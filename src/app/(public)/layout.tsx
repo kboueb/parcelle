92a99e629e7a9907c798a077427003024ba7cde9
Kani Bouebassihou <kanibouebassihou@gmail.com>
2026-07-24 14:15:17 +0000

    fix

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,6 +4,8 @@ import { Footer } from "@/components/public/Footer"
 import { BannerHero } from "@/components/public/BannerHero"
 import { getSiteName } from "@/lib/site-name"
 
+export const dynamic = "force-dynamic"
+
 export default async function PublicLayout({ children }: { children: React.ReactNode }) {
   const siteName = await getSiteName()
   return (
